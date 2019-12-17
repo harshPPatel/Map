fb4684284f74bc26ccb627117f5e792d6c38ccdc
Harsh Patel <harshppatel2880@gmail.com>
2019-12-17 16:08:21 -0600

    Recreated the project with webpack

---
+++ source/index.js
@@ -0,0 +1,5 @@
+// Importing app.js
+import './js/app';
+
+// Importing Styles
+import './scss/app.scss';
---
+++ source/js/app.js
@@ -0,0 +1,32 @@
+import Leaflet from 'leaflet';
+// import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
+
+// Creating map
+const map = Leaflet.map('mapid')
+  .setView([51.505, -0.09], 13);
+
+// Marker
+let marker;
+
+// Initializing Map
+Leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  maxZoom: 18,
+  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  id: 'mapbox/streets-v11',
+})
+  .addTo(map);
+
+// Handles the click event of map
+const onMapClicked = (e) => {
+  if (marker) {
+    map.removeLayer(marker);
+  }
+
+  marker = Leaflet.marker([e.latlng.lat, e.latlng.lng])
+    .addTo(map)
+    .bindPopup(`You clicked the map at ${e.latlng.toString()}`)
+    .openPopup();
+};
+
+// Adding click event to Map
+map.on('click', onMapClicked);
